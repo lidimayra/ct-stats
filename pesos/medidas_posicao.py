2bffe5c843e73d293aa881feff1605c3c59d953e
Lidiane Taquehara <lidi.mayra@gmail.com>
2016-05-23 01:02:13 -0300

    refactor de medidas de posição

--- pesos/medidas_posicao.py
+++ pesos/medidas_posicao.py
@@ -6,7 +6,7 @@ Created on Sun May 22 20:43:24 2016
 """
 
 from math import floor, ceil
-from pesos import carregarLista, frequencia_abs, universo_amostral, frequencia_acumulada
+from frequencias import carregarLista, frequencia_abs, universo_amostral, frequencia_acumulada
 
 def media(lista):
     xifi = frequencia_abs(lista)    
@@ -16,26 +16,22 @@ def media(lista):
     return float(somatoria_xifi) / universo_amostral(lista)
 
 def mediana(lista):
+    x1 = 0; x2 = 0
     fac = frequencia_acumulada(lista)
     em = (universo_amostral(lista) + 1) / 2.0        
-    
+
     for xi, freq in fac.items():
-        
         if (freq <= em):
-            if (em % 1 == 0):
-                res = xi
-            else:
-                posicao_1 = floor(em)
-                posicao_2 = ceil(em)
+            posicao_1 = floor(em)
+            posicao_2 = ceil(em)
 
-                for chave, valor in fac.items():
-                    if valor == posicao_1:
-                        x1 = chave
-                    elif valor == posicao_2:
-                        x2 = chave
-                
-                res = (x1 + x2) / 2.0
-
+    for chave, valor in fac.items():
+        if x1 == 0 and valor >= posicao_1:
+            x1 = chave
+        if x2 == 0 and valor >= posicao_2:
+            x2 = chave
+    
+    res = (x1 + x2) / 2.0
     return res
 
 def moda(lista):
@@ -45,7 +41,7 @@ def moda(lista):
 arquivo = 'pesos.txt'
 
 lista = carregarLista(arquivo)
-print(universo_amostral(lista))
+# lista.append(76)
 print media(lista)
 print(mediana(lista))
 print(moda(lista))
